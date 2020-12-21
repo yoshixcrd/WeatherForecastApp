dfe4891770e6926e3214403621fab97aa15f2ec5
fernandocalheirox <crd.yoshi@gmail.com>
2020-12-21 10:22:10 -0300

    0.2v

---
+++ lib/bloc/weather.bloc.dart
@@ -0,0 +1,98 @@
+
+class BlocWeather {
+  String getBackground(var value) {
+    switch(value){
+      case "storm":
+        return "assets/background/bg-tempestade.png";
+        break;
+      case "rain":
+        return "assets/background/bg-chuvoso.png";
+        break;
+      case "clear_day":
+        return "assets/background/bg-ensolarado.png";
+        break;
+      case "cloud":
+        return "assets/background/bg-nublado.png";
+        break;
+      case "cloudly_day":
+        return "assets/background/bg-nublado.png";
+        break;
+      case "cloudly_night":
+        return "assets/background/bg-nublado.png";
+        break;
+      case "clear_night":
+        return "assets/background/bg-noite.png";
+        break;
+
+      case "none_day":
+        return "assets/background/bg-ensolarado.png";
+        break;
+
+      case "none_night":
+        return "assets/background/bg-noite.png";
+        break;
+
+    }
+  }
+  
+
+  String getImage(var value) {
+    switch(value) {
+      case "storm":
+        return "assets/image/tempestade.png";
+        break;
+      case "rain":
+        return "assets/image/chuva.png";
+        break;
+      case "clear_day":
+        return "assets/image/sol-pequeno.png";
+        break;
+      case "cloud":
+        return "assets/image/sol-com-nuvem.png";
+        break;
+      case "cloudly_day":
+        return "assets/image/sol-com-nuvem.png";
+        break;
+      case "cloudly_night":
+        return "assets/image/sol-com-nuvem.png";
+        break;
+      case "clear_night":
+        return "assets/image/noite.png";
+        break;
+
+      case "none_day":
+        return "assets/image/sol-pequeno.png";
+        break;
+
+      case "none_night":
+        return "assets/image/noite.png";
+        break;
+    }
+  }
+
+  String getDay(String value) {
+    switch(value) {
+      case "Dom":
+        return "Domingo";
+        break;
+      case "Seg":
+        return "Segunda";
+        break;
+      case "Ter":
+        return "Terça";
+        break;
+      case "Qua":
+        return "Quarta";
+        break;
+      case "Qui":
+        return "Quinta";
+        break;
+      case "Sex":
+        return "Sexta";
+        break;
+      case "Sab":
+        return "Sabado";
+        break;
+    }
+  }
+}

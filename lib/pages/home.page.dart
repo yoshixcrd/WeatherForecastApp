44c804c2d721816d511cbff0a0cb3472886844ed
fernandocalheirox <crd.yoshi@gmail.com>
2021-01-02 13:13:16 -0300

    remove Include e add Navigator

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,14 +1,9 @@
 import 'package:PrevisaoDoTempo/bloc/weather.bloc.dart';
 import 'package:PrevisaoDoTempo/models/weatherAPI.model.dart';
 import 'package:PrevisaoDoTempo/pages/loading.page.dart';
-import 'package:PrevisaoDoTempo/widgets/futureCityName.widget.dart';
-import 'package:PrevisaoDoTempo/widgets/futureDate.widget.dart';
-import 'package:PrevisaoDoTempo/widgets/futureDescription.widget.dart';
-import 'package:PrevisaoDoTempo/widgets/futureHour.widget.dart';
-import 'package:PrevisaoDoTempo/widgets/futureImage.widget.dart';
-import 'package:PrevisaoDoTempo/widgets/futureMaxTemp.widget.dart';
-import 'package:PrevisaoDoTempo/widgets/futureMinTemp.widget.dart';
-import 'package:PrevisaoDoTempo/widgets/futureWeakDay.widget.dart';
+import 'package:PrevisaoDoTempo/pages/weather.page.dart';
+
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -28,63 +23,17 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    weather_api = fetchWeatherAPI();
     start();
-    
   }
-  Future start() async {
-    if(mounted) {
-      setState(() { completed = true; });
-    }
+  void start(){
+    weather_api = fetchWeatherAPI();
+  }
  
-    
-  }
   @override
   Widget build(BuildContext context) {
-    if(completed)
-      return Scaffold(
-        // Muda o background de acordo com a condição do clima
-        body: FutureBuilder<Weather_API>(
-          future: weather_api,
-          builder: (context, snapshot) {
-            if(snapshot.hasData) {
-              return Container(
-                  // Background Img
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      // Image Background Do Clima 
-                      image: AssetImage(bloc.getBackground(snapshot.data.results.forecast[0].condition)),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Scaffold(
-                    backgroundColor: Colors.transparent,
-                    body: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          SizedBox( height: 50), 
-                          futureCityName(context, weather_api),
-                          SizedBox(height: 10),
-                          futureWeakDay(context, weather_api, bloc),
-                          SizedBox(height: 5),
-                          futureDate(context, weather_api, bloc),
-                          SizedBox(height: 5),
-                          futureHour(context, weather_api),
-                          futureImage(context, weather_api, bloc),
-                          futureDescription(context, weather_api, bloc),
-                          SizedBox(height: 20),
-                          futureMaxTemp(context, weather_api),
-                          futureMinTemp(context, weather_api),
-                        ],   
-                      ),
-                    ),
-                  ),
-              );
-            } else{
-                return LoadingPage();
-              } 
-          }
-        ),
-      );
-    }
+    // Quando pegar os dados, chama a pagina Weather e passa os dados da api.
+    weather_api.whenComplete(() => Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=>WeatherPage(weather_api: weather_api)),(Route<dynamic> route) => false));
+    
+   return LoadingPage();
+  }
 }
